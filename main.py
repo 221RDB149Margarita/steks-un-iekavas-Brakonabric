b4df8082dd3c98ee6cc5a4a7c6c7a0c96d9137d6
Linass <dizzneyk@gmail.com>
2023-02-18 00:13:00 +0200

    commit

--- main.py
+++ main.py
@@ -29,19 +29,16 @@ def main():
     print("[!] \tUse an input to choose files or input - F or I ?")
     text = input(">:: \t")
     if text == "F":
-        try:
-            print("[!] \tEnter file name or file path. For example 'test/0'.")
-            fileName = input(">:: \t")
-            with open(fileName, "r") as file:
-                sourceText = file.read().strip()
-        except:
-            print("[Err] \tFile not found.")
+        print("[!] \tEnter file name or file path. For example 'test/0'.")
+        fileName = input(">:: \t")
+        with open(fileName, "r") as file:
+            text = file.read().strip()
     elif text == "I":
         print("[!] \tEnter text below.")
-        sourceText = input(">:: \t").strip()
+        text = input(">:: \t").strip()
     else:
         print("[Err] \tWrong input.")
-    mismatch = find_mismatch(sourceText)
+    mismatch = find_mismatch(text)
     print(mismatch)
 
 
